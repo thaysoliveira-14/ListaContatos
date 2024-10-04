111ad4c20aa9577ea72eae8d102f2366e64b15e6
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-10-03 22:47:34 -0300

    criando estado global

---
+++ lista-contatos/src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactReducer from './contactSlice';
+
+export const store = configureStore({
+    reducer: {
+        contacts: contactReducer,
+},
+});
